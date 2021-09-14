f6def0af82f585fd17e16edd84be59d929079358
swathisnayak14 <swathisnayak.is19@sahydri.edu.in>
2021-09-14 11:51:39 +0530

    codes added

---
+++ Abundant.cpp
@@ -0,0 +1,19 @@
+//Program to check whether a number is an Abundant Number or not
+
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+    int num,rem,sum=0;
+    cin>>num;
+    for(int i=1;i<num;i++){
+        rem=num%i;
+        if(rem==0)
+            sum+=i;
+    }
+    if(sum>num)
+        cout<<num<< " is an abundant number";
+    else
+        cout<<num<< " is not an abundant number";
+
+    return 0;
+}
