d43ae62ec4d0c877b895330356add3b6040a8665
swathisnayak14 <swathisnayak.is19@sahydri.edu.in>
2021-09-20 10:45:35 +0530

    Array Frequency Updated

--- ArrayFrequency.cpp
+++ ArrayFrequency.cpp
@@ -1,3 +1,23 @@
 //program to find the frequency of element in an array
 
-//doubt
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main()
+{
+    set<int> s;
+    int n;
+    cin >> n;
+    int a[n];
+    for (int i = 0; i < n; i++)
+    {
+        cin >> a[i];
+        // 12 9 2 12 3
+        // int num = a[i];
+        s.insert(a[i]);
+        // 12 9 2 3 
+    }
+    cout<<s.size()<<endl;//counting the distinct elements
+    return 0;
+}
